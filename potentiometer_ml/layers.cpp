17013290db72ad45ce6417bd2ffca3a069f5c858
SungWookKang <kpuppy110794@gmail.com>
2023-01-30 16:37:17 +0900

    Make nn_relu class

--- potentiometer_ml/layers.cpp
+++ potentiometer_ml/layers.cpp
@@ -3,3 +3,16 @@
 #include <cmath>
 
 using namespace std;
+
+void nn_relu::forward(double* input_sum)
+{
+	int col = sizeof(input_sum) / sizeof(double);
+
+	for (int col_index = 0; col_index < col; col_index)
+	{
+		if (input_sum[col_index] < 0.0)
+		{
+			input_sum[col_index] = 0;
+		}
+	}
+}
